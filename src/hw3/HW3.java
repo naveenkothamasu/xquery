ced4c02b5ea5cfaa3cced808b5de29cf3fff116c
Naveen <naveenkothamasu@gmail.com>
2014-04-25 16:22:20 -0700

    xquery

---
+++ src/hw3/HW3.java
@@ -0,0 +1,72 @@
+package hw3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import oracle.jdbc.OracleResultSet;
+import oracle.xdb.XMLType;
+
+
+public class HW3 {
+
+	public static Connection mainConnection = null;
+	public static Statement mainStatement = null;
+	public static ResultSet rs = null;
+
+	public static void getConnectionToDB() {
+		try {
+
+			// loading Oracle Driver
+			System.out.print("Looking for Oracle's jdbc-odbc driver ... ");
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			System.out.println(", Loaded.");
+
+			String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+			String userName = "Scott";
+			String password = "tiger";
+
+			System.out.print("Connecting to DB...");
+			mainConnection = DriverManager.getConnection(URL, userName,
+					password);
+			System.out.println(", Connected!");
+
+			mainStatement = mainConnection.createStatement();
+
+		} catch (Exception e) {
+			System.out.println("Error while connecting to DB: " + e.toString());
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	public static void createTables() throws SQLException{
+		mainStatement.execute("CREATE TABLE BOOK ( BOOK_ID  NUMBER PRIMARY KEY, BOOK_DATA xmltype ) ");
+		mainStatement.execute("CREATE TABLE REVIEW ( REVIEW_ID  NUMBER PRIMARY KEY, REVIEW_DATA xmltype ) ");
+	}
+	public static void insertRecords() throws SQLException, FileNotFoundException{
+		Scanner s = new Scanner(new File("C:\\Users\\NaveenKumar\\Desktop\\585\\HW3\\DDLDML.sql"));
+		while(s.hasNext()){
+			mainStatement.executeQuery(" "+ s.nextLine()+ " ");
+		}
+	}
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
+		getConnectionToDB();
+		createTables();
+		insertRecords();
+		/*
+		ResultSet rs = mainStatement.executeQuery("SELECT * FROM BOOK");
+		OracleResultSet orset = (OracleResultSet) rs; 
+		
+		while(orset.next()){
+			XMLType poxml = (XMLType)orset.getObject(2); 
+			System.out.println(poxml.getStringVal());
+		}
+		*/
+	}
+}
